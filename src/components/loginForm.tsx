fedd79a3c8ef8e4c166e6bd315ba59155ddb101e
Conor Mullan <conor.mullan@unosquare.com>
2022-09-01 17:04:04 +0100

    popups-suggestions

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -1,46 +1,49 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { CreateUser } from "../types/users.types";
-import axios from "axios";
-import Card from "@mui/material/Card";
-import "../pages/css/login.css";
-import { Stack } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
-import Auth from "./authorisation/context";
-import PopUp from "./popup";
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+import {CreateUser} from '../types/users.types';
+import axios from 'axios';
+import Card from '@mui/material/Card';
+import '../pages/css/login.css';
+import {Stack} from '@mui/material';
+import {Link, useNavigate} from 'react-router-dom';
+import Auth from './authorisation/context';
+import PopUp from './popup';
 
 const LoginForm = () => {
   const context = React.useContext(Auth.AuthContext);
   const navigate = useNavigate();
-  const [status, setStatus] = useState<string>("Test message");
+  const [status, setStatus] = useState<string>('');
   const [open, setOpen] = React.useState(false);
 
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: {errors},
   } = useForm<CreateUser>();
 
-  const toggleOpen = () => {
-    if (open) {
-      setOpen(false);
-    } else {
-      setOpen(true);
-    }
-  };
+  /* Dont need the below snippet, open and the click is handled within the PopUp class
 
-  const handleClick = () => {
-    toggleOpen();
-  };
+          const toggleOpen = () => {
+            if (open) {
+              setOpen(false);
+            } else {
+              setOpen(true);
+            }
+          };
+          const handleClick = () => {
+            toggleOpen();
+          };
+  */
 
   const onSubmit = (data: any) => {
+    //Reset status back to empty string otherwise the previous status may show for a split second before it updates
+    setStatus('');
     axios
-      .post("http://localhost:5001/api/auth/userLogin", data, {
-        headers: { "Content-Type": "application/json" },
+      .post('http://localhost:5001/api/auth/userLogin', data, {
+        headers: {'Content-Type': 'application/json'},
       })
       .then((response) => {
-        setStatus("Successful login! Redirecting...");
-        toggleOpen();
+        setStatus('Successful login! Redirecting...');
         if (context) {
           context.updateUserSession({
             accessToken: response.data[0],
@@ -48,12 +51,11 @@ const LoginForm = () => {
           });
         }
         setTimeout(() => {
-          navigate("/");
+          navigate('/');
         }, 2000);
       })
       .catch((error) => {
-        setStatus("Login details invalid.");
-        toggleOpen();
+        setStatus('Login details invalid.');
         console.log(error.data);
       });
   };
@@ -69,20 +71,23 @@ const LoginForm = () => {
                 <label>Username</label>
                 <input
                   className="input-field"
-                  {...register("userName")}
+                  {...register('userName')}
                   required={true}
                   type="text"
                 />
                 <label>Password</label>
                 <input
                   className="input-field"
-                  {...register("userPassword")}
+                  {...register('userPassword')}
                   required={true}
                   type="password"
                 />
-                <PopUp message={status} open={open} />
+                {
+                  //Pass down setOpen as a prop, means we can use a single state for all open handling
+                }
+                <PopUp message={status} open={open} setOpen={setOpen} />
                 <h5>
-                  Don't have an account?{" "}
+                  Don't have an account?
                   <Link to="../register">Register here</Link>
                 </h5>
               </Stack>
@@ -94,4 +99,4 @@ const LoginForm = () => {
   );
 };
 
-export { LoginForm };
+export {LoginForm};
