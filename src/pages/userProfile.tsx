012c2f81d1862a56c1c593078b19b75b8932a029
Wren00 <lorenwalsh00@gmail.com>
2022-09-06 00:42:07 +0100

    Adding commit for editing profile message, working but requires some refining!

--- src/pages/userProfile.tsx
+++ src/pages/userProfile.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import "./css/profile.css";
 import { UserProfile } from "../types/userProfile.types";
 import { useParams } from "react-router-dom";
@@ -12,13 +12,15 @@ import Auth from "../components/authorisation/context";
 import jwtDecode from "jwt-decode";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+import { SaveOutlined } from "@mui/icons-material";
 
 const UserProfilePage = () => {
   let { id } = useParams();
   let parsedId: number = 0;
   const [user, setUser] = useState<User>();
   const [profile, setProfile] = useState<UserProfile>();
-
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const [profileMessage, setProfileMessage] = useState<string>();
   //check user id matches currently logged in user
 
   const context = React.useContext(Auth.AuthContext);
@@ -40,6 +42,43 @@ const UserProfilePage = () => {
     isLoggedIn = true;
   }
 
+  const updateProfileMessage = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setProfileMessage(event.target.value);
+  };
+
+  const toggleEditMode = () => {
+    setIsEditMode(true);
+  };
+
+  const saveChanges = () => {
+    if (profile && profileMessage) {
+      console.log(profileMessage);
+      console.log(parsedId);
+      const updatedUserProfile: UserProfile = {
+        profileId: parsedId,
+        profileMessage: profileMessage,
+        profilePicture: profile?.profilePicture,
+      };
+
+      axios
+        .put(
+          "http://localhost:5001/api/users/updateUserProfile",
+          updatedUserProfile,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => console.log(error));
+    }
+    setIsEditMode(false);
+    //Axios update user profile
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       const { data: response } = await axios.get(
@@ -56,7 +95,7 @@ const UserProfilePage = () => {
       const { data: response } = await axios.get(
         `http://localhost:5001/api/users/getProfileByUserId/` + id
       );
-      console.log(response);
+      console.log("setting user", response);
       setProfile(response);
     };
     fetchData();
@@ -86,13 +125,30 @@ const UserProfilePage = () => {
         <Grid item className="message" md={6} sm={12}>
           {profile?.profileMessage}
           {isLoggedIn && (
-            <div className="div-with-edit-button">
-              <Tooltip title="Edit message">
-                <Button size="small" color="primary">
-                  <EditIcon />
-                </Button>
-              </Tooltip>
-            </div>
+            <>
+              {isEditMode ? (
+                <TextField
+                  value={profileMessage}
+                  onChange={(e) => updateProfileMessage(e)}
+                  id="outlined-basic"
+                  variant="outlined"
+                />
+              ) : (
+                <div>{profileMessage}</div>
+              )}
+              <div className="div-with-edit-button">
+                <Tooltip title="Edit message">
+                  <Button size="small" color="primary" onClick={toggleEditMode}>
+                    <EditIcon />
+                  </Button>
+                </Tooltip>
+                <Tooltip title="Save message">
+                  <Button size="small" color="primary" onClick={saveChanges}>
+                    <SaveOutlined />
+                  </Button>
+                </Tooltip>
+              </div>
+            </>
           )}
         </Grid>
       </Grid>
