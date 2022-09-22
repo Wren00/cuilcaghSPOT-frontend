c729d5223c3ea06c3581de037bd079d5acd9e03e
Wren00 <lorenwalsh00@gmail.com>
2022-09-22 14:42:49 +0100

    user settings page created

---
+++ src/pages/settings.tsx
@@ -0,0 +1,11 @@
+import UserSettings from "../components/userSettings";
+
+const Settings = () => (
+  <div className="settings-page">
+    <div>
+      <UserSettings />
+    </div>
+  </div>
+);
+
+export default Settings;
